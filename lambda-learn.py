9ebfb254454ccf151f2fc4f17c62e1d65bbd880d
Enkhbat Baljnyam <ad.baljnyam@gmail.com>
2019-03-25 20:42:17 +1100

    Add files via upload

--- lambda-learn.py
+++ lambda-learn.py
@@ -1,2 +1,28 @@
 x = lambda a, b, c : a + b + c
-print(x(5, 6, 2))
+print(x(5, 9, 2))
+
+x = lambda a, b : a * b
+print(x(5, 8))
+
+x = lambda a, b, c: a + b + c
+
+print(x(5,6,7))
+
+def myfunc(n):
+       return lambda a : a * n
+
+mydoubler = myfunc(2)
+
+print(mydoubler(1))
+
+def myfunc(n):
+       return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
+
+#Create a lambda function that takes one parameter (a) and returns it.
+x = labda a : a
